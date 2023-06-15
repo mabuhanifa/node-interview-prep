5237b3dac561bbdb5dc59ad0b6ee00d65bcecbe4
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-15 22:25:24 +0600

    index html

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require("path");
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}...`));
