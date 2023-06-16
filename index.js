f56416466425ef77dd0a74e6548ea0f3a3a3db5d
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-16 16:26:03 +0600

    middleware

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}}`);
+  next();
+});
+
 // 'content-type: application/x-www-form-urlencoded'
 app.use(express.urlencoded({ extended: false }));
 // static files
