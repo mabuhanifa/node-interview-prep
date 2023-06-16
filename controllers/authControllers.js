ea304d948e6ac3e94f3028f4828a52c92085122b
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-16 20:37:46 +0600

    jwt tokens

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const userDB = {
   users: require("../data/user.json"),
   setUsers: function (data) {
@@ -17,13 +19,27 @@ const handleLogin = async (req, res) => {
       .json({ message: "Username and password are required." });
 
   const foundUser = userDB.users.find((person) => person.username === user);
-  
+
   if (!foundUser) return res.sendStatus(401); //Unauthorized
   // evaluate password
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
+    
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expireIn: "30s" }
+    );
+
+    const resfreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expireIn: "1d" }
+    );
+
     res.json({
-        //create JWTs here
+      //create JWTs here
+
       success: `User ${foundUser.username} is logged in`,
     });
   } else {
