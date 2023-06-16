76d767b89a7941f5088700c2f8d63ae047d5ad7c
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-16 17:28:18 +0600

    handleNewUser

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -4,3 +4,40 @@ const userDB = {
     this.users = data;
   },
 };
+
+const fsPromises = require("fs").promises;
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+const handleNewUser = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: "Username and password are required." });
+
+  // check for duplicate usernames in the db
+  const duplicate = userDB.users.find((person) => person.username === user);
+  if (duplicate) return res.sendStatus(409); //Conflict
+
+  try {
+    //encrypt the password
+    const hashedPwd = await bcrypt.hash(pwd, 10);
+
+    //store the new user
+    const newUser = { username: user, password: hashedPwd };
+    userDB.setUsers([...userDB.users, newUser]);
+
+    //write the db file
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "data", "user.json"),
+      JSON.stringify(userDB.users)
+    );
+
+    console.log(userDB.users);
+
+    res.status(201).json({ success: `New user ${user} created!` });
+  } catch (err) {
+    console.log(err);
+  }
+};
