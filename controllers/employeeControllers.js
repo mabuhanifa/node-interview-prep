90d00d7f3d711da6d09d267e82d20e799841e2a1
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-17 00:34:53 +0600

    createNewEmployee

--- controllers/employeeControllers.js
+++ controllers/employeeControllers.js
@@ -1,7 +1,34 @@
-const employees = require("../data/employees.json");
-
-const getEmployees = (req, res) => {
-  res.send(employees);
+const data = {
+  employees: require("../data/employees.json"),
+  setEmployees: function (data) {
+    this.employees = data;
+  },
 };
 
-module.exports = { getEmployees };
+const getAllEmployees = (req, res) => {
+  res.json(data.employees);
+};
+
+const createNewEmployee = (req, res) => {
+  const newEmployee = {
+    id: data.employees?.length
+      ? data.employees[data.employees.length - 1].id + 1
+      : 1,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+  };
+
+  if (!newEmployee.firstname || !newEmployee.lastname) {
+    return res
+      .status(400)
+      .json({ message: "First and last names are required." });
+  }
+
+  data.setEmployees([...data.employees, newEmployee]);
+  res.status(201).json(data.employees);
+};
+
+module.exports = {
+  getAllEmployees,
+  createNewEmployee,
+};
--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,9 +1,16 @@
-const { getEmployees } = require("../controllers/employeeControllers");
+const ROLES_LIST = require("../config/roles_list");
+const {
+  getAllEmployees,
+  createNewEmployee,
+} = require("../controllers/employeeControllers");
 const verifyJWT = require("../middleware/verifyJWT");
 const verifyRoles = require("../middleware/verifyRoles");
 
 const router = require("express").Router();
 
-router.route("/").get(verifyRoles(),verifyJWT, getEmployees);
+router
+  .route("/")
+  .get(verifyRoles(ROLES_LIST.Admin), verifyJWT, getAllEmployees)
+  .post(ROLES_LIST.Admin, verifyJWT, createNewEmployee);
 
 module.exports = router;
